2a0be879b2ec37f749eadd9828e52ba8ea667032
Nancy Lee <nancy.lee@alexsolutions.com.au>
2021-01-08 13:50:48 +1100

    list videos and individual videos

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import {
+    Route,
+    BrowserRouter as Router,
+    Switch,
+} from "react-router-dom";
+import Home from './Home';
+import Player from './Player';
 import './App.css';
+
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-            <video controls muted>
-                <source src="http://localhost:4000/video" type="video/mp4"></source>
-            </video>
-            </header>
-        </div>
+        <Router>
+            <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/player/:id" component={Player}></Route>
+            </Switch>
+        </Router>
     );
 }
 export default App;
