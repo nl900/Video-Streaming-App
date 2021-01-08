6fecc5b5fb765ec063a45fed5b1348430c41133e
Nancy Lee <nancy.lee@alexsolutions.com.au>
2021-01-08 13:58:32 +1100

    add header and footer

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+
 export default class Home extends Component {
     constructor() {
         super();
@@ -19,23 +22,25 @@ export default class Home extends Component {
     render() {
         return (
             <div className="App App-header">
-                <div className="container">
-                    <div className="row">
-                        {this.state.videos.map(video =>
-                        <div className="col-md-4" key={video.id}>
-                            <Link to={`/player/${video.id}`}>
-                                <div className="card border-0">
-                                    <img src={`http://localhost:4000${video.poster}`} alt={video.name} />
-                                    <div className="card-body">
-                                        <p>{video.name}</p>
-                                        <p>{video.duration}</p>
+                <Header />
+                    <div className="container">
+                        <div className="row">
+                            {this.state.videos.map(video =>
+                            <div className="col-md-4" key={video.id}>
+                                <Link to={`/player/${video.id}`}>
+                                    <div className="card border-0">
+                                        <img src={`http://localhost:4000${video.poster}`} alt={video.name} />
+                                        <div className="card-body">
+                                            <p>{video.name}</p>
+                                            <p>{video.duration}</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </Link>
+                                </Link>
+                            </div>
+                            )}
                         </div>
-                        )}
                     </div>
-                </div>
+                <Footer />
             </div>
         )
     }
