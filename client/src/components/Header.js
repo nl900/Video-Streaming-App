3ee717011985e656b7fc7a564bce1a3c9bdf143b
Nancy Lee <nancy.lee@alexsolutions.com.au>
2021-02-03 09:39:54 +1100

    navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,26 @@
-import React from 'react';
-function Header() {
-    return (
-        <header>
-            Video Streaming App
-        </header>
-    );
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, NavbarBrand, NavbarToggler} from 'reactstrap';
+
+export default class Header  extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isOpen: false};
+        this.toggle = this.toggle.bind(this);
+      }
+      
+      toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+      }
+
+    render () {
+        return (        
+            <Navbar className="justify-content-center" color="dark" dark expand="md">
+                <NavbarBrand tag={Link} to="/">Video Streaming App</NavbarBrand>
+                <NavbarToggler onClick={this.toggle}/>
+            </Navbar>
+        );  
+    }
 }
-export default Header;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Navbar, NavbarBrand, NavbarToggler} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
